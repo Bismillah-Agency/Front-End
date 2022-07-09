315ca282d0fc7c1209329643c86bae3bc87e8fd3
mumtazdedi <mumtazdedi@gmail.com>
2022-07-09 21:12:42 +0700

    fixing scroll in testimonial card

--- src/components/Landing/TestimonialCard/index.jsx
+++ src/components/Landing/TestimonialCard/index.jsx
@@ -17,37 +17,37 @@ const TestimonialCard = (props) => {
     const whiteStars = [];
 
     for (let index = 0; index < props.userRating; index++) {
-        yellowStars.push(<img src={YellowStars} className='h-5 w-5'/>)
+        yellowStars.push(<img src={YellowStars} alt="star" className='h-5 w-5' />)
     }
     for (let index = 0; index < 5 - props.userRating; index++) {
-        yellowStars.push(<img src={WhiteStars} className='h-5 w-5'/>)
+        yellowStars.push(<img src={WhiteStars} alt="star" className='h-5 w-5' />)
     }
 
-  return (
-    <div className='flex flex-col sm:flex-row gap-6 h-fit'>
-        <div className='hidden sm:block sm:h-[240px] sm:w-[240px] md:h-[280px] md:w-[280px] rounded-2xl overflow-hidden '>
-            <img className='' src={props.userImg}/>
-        </div>
-        <div className='flex flex-col gap-6 sm:gap-0 sm:justify-between sm:w-[592px]'>
-            <div className='flex flex-col gap-2'>
-                <div className='flex mr-1'>
-                    {yellowStars}
-                    {whiteStars}
-                </div>
-                <p className='text-xl text-slate-400'>{props.userReview}</p>
+    return (
+        <div className='flex flex-col sm:flex-row gap-6 h-fit'>
+            <div className='hidden sm:block sm:h-[240px] sm:w-[240px] md:h-[280px] md:w-[280px] rounded-2xl'>
+                <img className='rounded-2xl' src={props.userImg} alt={props.userName} />
             </div>
-            <div className='flex gap-3 items-center'>
-                <div className='sm:hidden h-12 w-12 rounded-xl overflow-hidden '>
-                    <img className='' src={props.userImg}/>
+            <div className='flex flex-col gap-6 sm:gap-0 sm:justify-between sm:w-[592px]'>
+                <div className='flex flex-col gap-2'>
+                    <div className='flex mr-1'>
+                        {yellowStars}
+                        {whiteStars}
+                    </div>
+                    <p className='text-xl text-slate-400'>{props.userReview}</p>
                 </div>
-                <div>
-                    <p className='text-xl font-semibold text-slate-700'>{props.userName}</p>
-                    <p className='text-slate-400'>{props.userDetail}</p>
+                <div className='flex gap-3 items-center'>
+                    <div className='sm:hidden h-12 w-12 rounded-xl overflow-hidden '>
+                        <img className='' src={props.userImg} alt={props.userName} />
+                    </div>
+                    <div>
+                        <p className='text-xl font-semibold text-slate-700'>{props.userName}</p>
+                        <p className='text-slate-400'>{props.userDetail}</p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default TestimonialCard
